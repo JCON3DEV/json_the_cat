93f704a03ec5b94a67a68f87a24d4c939fe9ac2e
JCON3DEV <jconheasman@gmail.com>
2020-07-30 22:39:00 +0000

    First commit HTTP get request and command line arguments as input

---
+++ breedFetcher.js
@@ -0,0 +1,17 @@
+const request = require("request");
+const breedName = process.argv.slice(2);
+
+
+// https://api.thecatapi.com/v1/images/search
+// https://api.thecatapi.com/v1/breeds/search
+request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+  // console.log('ERROR: Something Went Wrong', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  // console.log('body:', body); // Print the HTML for the Google homepage.
+  const data = JSON.parse(body);
+  
+  console.log(data);
+  // console.log(typeof data);
+});
+
+// console.log(typeof body)
